740b48ea9d9e0d977cd66d19381d67a8288124e6
hoatd2212 <trandinhhoa203@gmail.com>
2025-05-09 16:32:00 +0700

    #2 - maptable module

---
+++ maptable.h
@@ -0,0 +1,18 @@
+#ifndef MAPTABLE_H
+#define MAPTABLE_H
+
+#define FILE_SIZE 100
+#define MAX_FILES 10
+
+struct file_mapping_entry {
+    char file_name[50];
+    void *mapped_address;
+    int fd;
+};
+
+extern struct file_mapping_entry file_mapping_table[MAX_FILES];
+extern int table_index;
+
+void add_file_mapping_entry(const char *file_name, void *mapped_address, int fd);
+
+#endif 
